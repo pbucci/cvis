8c699a9ad62697e35fd1f97691589be99bbfa73d
pbucci <pbucci@gmail.com>
2014-11-13 18:58:56 -0800

    cleanup'

--- vis/vis.py
+++ vis/vis.py
@@ -8,30 +8,54 @@ from collections import OrderedDict
 
 import fakedata as fd
 
+#-------------------------------------------------------------------------------
 corpus = fd.corpus
 texts  = corpus.keys()
 color  = '#96fa00'
 tools  ="hover"
 texts  = [key for key,value in fd.corpus.items()]
+#-------------------------------------------------------------------------------
 
+# file to output to
 output_file("vis.html")
-
+figure()
+#-------------------------------------------------------------------------------
+# grid values
 x = map(float, sorted(range(1,13)*8))
 y = map(float, range(1,9)*12)
-w = [0.95]*96
-h = [0.95]*96
+w = [0.90]*96
+h = [0.90]*96
+#-------------------------------------------------------------------------------
 
+#-------------------------------------------------------------------------------
 source = ColumnDataSource(
     data=dict(
         id=texts,
     )
 )
+#-------------------------------------------------------------------------------
 
-rect(x,y,w,h,source=source,tools=tools)
 
+#-------------------------------------------------------------------------------
+rect(   x,y,w,h,
+        source=source,
+        tools=tools,
+        title=None,
+        line_color=None,
+        plot_width=200,
+        plot_height=200)
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+# graph options
+grid().grid_line_color = None
+axis().axis_line_color = None
+axis().major_label_text_font_size = "0pt"
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
 hover = curplot().select(dict(type=HoverTool))
-hover.tooltips = OrderedDict([
-    ('id', '@id'),
-])
+hover.tooltips = OrderedDict([('id', '@id'),])
+#-------------------------------------------------------------------------------
 
 show()
